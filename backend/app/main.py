a229aeafb0e51814becc685f1c64710ae37279bf
Siddhant Kuwar <siddhankuwar116@gmail.com>
2026-04-23 23:21:54 -0700

    started the API

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 
-from app.api.health import router as health_router
+from .api.health import router as health_router
 
 
-app = FastAPI(title="Battry API")
+app = FastAPI(title="Battry API v2")
 
 app.include_router(health_router, prefix="/api")
